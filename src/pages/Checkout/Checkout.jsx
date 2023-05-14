c51472128d598c3d1bcec7bbac2970f2d7199168
Rashed967 <rashedhasan967.com>
2023-05-14 09:32:36 +0600

    book added

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -4,18 +4,29 @@ import { useLoaderData } from "react-router-dom";
 
 const Checkout = () => {
     const service = useLoaderData()
-    console.log(service)
+    const {price, service_id, title, _id} = service;
+    
+    const handleCheckOut = e => {
+      e.preventDefault()
+      const form = e.target;
+      const name = form.name.value;
+      const date = form.date.value;
+      const email = form.email.value;
+      const price = form.price.value;
+      console.log(name, date, email, price)
+    }
     return (
         <div className=" bg-base-200 mx-auto pt-8">
 
    <div>
    <div className="mx-auto mt-9 card w-full md:max-w-screen-lg  shadow-2xl bg-base-100">
-      <div className="card-body grid md:grid-cols-2">
+      <form onSubmit={handleCheckOut} className="card-body ">
+        <div className="grid md:grid-cols-2 space-x-4">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="text" name="name" placeholder="email" className="input input-bordered" />
+          <input type="text" name="name" defaultValue={title} placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
@@ -27,18 +38,19 @@ const Checkout = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" name="email" placeholder="password" className="input input-bordered" />
+          <input type="email" name="email"  className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Price</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" name="price" defaultValue={`$ ${price}`} className="input input-bordered" />
         </div>
-      </div>
-        <div className="form-control p-7">
+        </div>
+        <div className="form-control p-7 w-full">
           <input className="btn btn-primary btn-block" type="submit" value="Submit" />
         </div>
+      </form>
     </div>
    </div>
 </div>
